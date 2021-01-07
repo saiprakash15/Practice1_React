6eeb36c34b985275c0c4acda35fd734ddf9d0796
saiprakash15 <dsaip96@gmail.com>
2021-01-07 18:39:59 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import './SeasonDisplay.css';
+import  Spinner  from "./Spinner";
+ 
+class App extends React.Component
+{
+    constructor( props)
+    {
+        super(props);
+        this.state  = { lat :null , errorMessage : ''};
+        
+    }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+componentDidMount()
+{
+    window.navigator.geolocation.getCurrentPosition(
+        position=>{this.setState({lat:position.coords.latitude});},
+        err => {this.setState({errorMessage:err.message});}
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
+
+    rendercontent()
+    {
+       if(this.state.errorMessage && !this.state.lat)
+        return <div>Error : { this.state.errorMessage}</div>;
+
+        if(!this.state.errorMessage && this.state.lat)
+        return <SeasonDisplay lat = { this.state.lat} /> ;
+         
+        return <Spinner />
+    }
+
+
+render()
+{
+    return(
+        <div className = "Border red"> {this.rendercontent()}        </div>   ) ;
+}
+}
+ReactDom.render(<App /> , document.querySelector('#root'));
